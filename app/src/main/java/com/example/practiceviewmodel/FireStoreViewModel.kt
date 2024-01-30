4b7afbd02c8005cca596e2aeaa1776b090d06520
Ahmad <khanyousafzaideveloper@gmail.com>
2024-01-30 10:14:25 +0500

    successfully test of storing data in firestore with viewModel

--- app/src/main/java/com/example/practiceviewmodel/FireStoreViewModel.kt
+++ app/src/main/java/com/example/practiceviewmodel/FireStoreViewModel.kt
@@ -1,42 +1,42 @@
-//package com.example.practiceviewmodel
-//
-//import android.content.ContentValues
-//import android.util.Log
-//import androidx.compose.runtime.getValue
-//import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.setValue
-//import androidx.lifecycle.ViewModel
-//import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.firestore.ktx.firestore
-//import com.google.firebase.ktx.Firebase
-//
-//class FireStoreViewModel: ViewModel() {
-//    var name by mutableStateOf("")
-//
-//    var university by mutableStateOf("")
-//
-//    var rollNo by mutableStateOf("")
-//
-//    var section by mutableStateOf("")
-//
-//    var firestore = Firebase.firestore
-//
-//    private val userMap = hashMapOf(
-//        "name" to name,
-//        "university" to university,
-//        "rollNo" to rollNo,
-//        "section" to section
-//    )
-//
-//    fun onClick(){
-//        firestore.collection("users")
-//            .add(userMap)
-//            .addOnSuccessListener { documentReference ->
-//                Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-//            }
-//            .addOnFailureListener { e ->
-//                Log.w(ContentValues.TAG, "Error adding document", e)
-//            }
-//    }
-//    //val userId = FirebaseAuth.getInstance().currentUser!!.uid
-//}
+package com.example.practiceviewmodel
+
+import android.content.ContentValues
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class FireStoreViewModel: ViewModel() {
+    var name by mutableStateOf("")
+
+    var university by mutableStateOf("")
+
+    var rollNo by mutableStateOf("")
+
+    var section by mutableStateOf("")
+
+    var firestore = Firebase.firestore
+
+
+    fun onClick(){
+        val userMap = hashMapOf(
+            "name" to name,
+            "university" to university,
+            "rollNo" to rollNo,
+            "section" to section
+        )
+        firestore.collection("users")
+            .add(userMap)
+            .addOnSuccessListener { documentReference ->
+                Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(ContentValues.TAG, "Error adding document", e)
+            }
+    }
+}
--- app/src/main/java/com/example/practiceviewmodel/fireStore.kt
+++ app/src/main/java/com/example/practiceviewmodel/fireStore.kt
@@ -1,80 +1,51 @@
 package com.example.practiceviewmodel
 
-import android.annotation.SuppressLint
-import android.content.ContentValues
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 
-@SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun FireStore( ) {
-
-
-    var name by remember {
-        mutableStateOf("")
-    }
-
-    var university by  remember {
-        mutableStateOf("")
-    }
-
-    var rollNo by remember {
-        mutableStateOf("")
-    }
-
-    var section by remember {
-        mutableStateOf("")
-    }
-
-    val firestore = Firebase.firestore
-
-    val userMap = hashMapOf(
-        "name" to name,
-        "university" to university,
-        "rollNo" to rollNo,
-        "section" to section
-    )
+fun FireStore(viewModel:FireStoreViewModel = viewModel()) {
 
     Column(modifier = Modifier.padding(16.dp)) {
         Text(text = "Name")
-        OutlinedTextField(value = name, onValueChange = { name = it })
+        OutlinedTextField(value = viewModel.name, onValueChange = { viewModel.name = it }, keyboardOptions = KeyboardOptions(
+            KeyboardCapitalization.Words))
         Text(text = "University")
         OutlinedTextField(
-            value = university,
-            onValueChange = { university = it })
+            value = viewModel.university,
+            onValueChange = { viewModel.university = it }, keyboardOptions = KeyboardOptions(
+                KeyboardCapitalization.Words)
+        )
         Text(text = "Roll No")
-        OutlinedTextField(value = rollNo, onValueChange = { rollNo = it })
+        OutlinedTextField(value = viewModel.rollNo, onValueChange = { viewModel.rollNo = it } , keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
         Text(text = "Section")
-        OutlinedTextField(value = section, onValueChange = { section = it })
+        OutlinedTextField(value = viewModel.section, onValueChange = { viewModel.section = it }, keyboardOptions = KeyboardOptions(
+            KeyboardCapitalization.Characters)
+        )
 
-        Button(onClick = { firestore.collection("users")
-            .add(userMap)
-            .addOnSuccessListener { documentReference ->
-                Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(ContentValues.TAG, "Error adding document", e)
-            } }) {
+        Button(onClick = { viewModel.onClick() }) {
             Text(text = "Push to Firestore")
         }
+
+        Text(text = "")
+        Text(text = "")
+        Text(text = "")
+        Text(text = "")
     }
 }
