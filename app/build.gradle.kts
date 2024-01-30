65edf21d6bfdb275aee7f875d1cd826fc577abde
Ahmad <khanyousafzaideveloper@gmail.com>
2024-01-30 09:58:09 +0500

    successfully test of storing data in firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,19 +53,24 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0-alpha02")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-firestore")
-    implementation ("androidx.navigation:navigation-compose:2.7.3")
+    implementation ("androidx.navigation:navigation-compose:2.7.6")
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("com.google.firebase:firebase-firestore:24.10.1")
 
--- app/src/main/java/com/example/practiceviewmodel/fireStore.kt
+++ app/src/main/java/com/example/practiceviewmodel/fireStore.kt
@@ -1,8 +1,8 @@
 package com.example.practiceviewmodel
 
-import android.content.ContentValues.TAG
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -12,36 +12,69 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import androidx.compose.runtime.saveable.rememberSaveable as rememberSaveable1
 
 
+@SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun FireStore(viewModel:FireStoreViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
+fun FireStore( ) {
 
+
+    var name by remember {
+        mutableStateOf("")
+    }
+
+    var university by  remember {
+        mutableStateOf("")
+    }
+
+    var rollNo by remember {
+        mutableStateOf("")
+    }
+
+    var section by remember {
+        mutableStateOf("")
+    }
+
+    val firestore = Firebase.firestore
+
+    val userMap = hashMapOf(
+        "name" to name,
+        "university" to university,
+        "rollNo" to rollNo,
+        "section" to section
+    )
 
     Column(modifier = Modifier.padding(16.dp)) {
         Text(text = "Name")
-        OutlinedTextField(value = viewModel.name, onValueChange = { viewModel.name = it })
+        OutlinedTextField(value = name, onValueChange = { name = it })
         Text(text = "University")
-        OutlinedTextField(value = viewModel.university, onValueChange = { viewModel.university = it })
+        OutlinedTextField(
+            value = university,
+            onValueChange = { university = it })
         Text(text = "Roll No")
-        OutlinedTextField(value = viewModel.rollNo, onValueChange = { viewModel.rollNo = it })
+        OutlinedTextField(value = rollNo, onValueChange = { rollNo = it })
         Text(text = "Section")
-        OutlinedTextField(value = viewModel.section, onValueChange = { viewModel.section = it })
+        OutlinedTextField(value = section, onValueChange = { section = it })
 
-        Button(onClick = { viewModel.onClick() }) {
+        Button(onClick = { firestore.collection("users")
+            .add(userMap)
+            .addOnSuccessListener { documentReference ->
+                Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(ContentValues.TAG, "Error adding document", e)
+            } }) {
             Text(text = "Push to Firestore")
         }
     }
-
-
-
 }
